f6ae861e4029c6c79e8312a0b1a2757affd1c62e
ramilzf <ramil.zeynalov@tudent.wsb.edu.pl>
2025-03-24 04:03:31 +0400

    Added latest code and Git screenshots

--- QuadraticSolver.Tests/obj/Debug/net9.0/QuadraticSolver.Tests.AssemblyInfo.cs
+++ QuadraticSolver.Tests/obj/Debug/net9.0/QuadraticSolver.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QuadraticSolver.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9bf0a99b3ee1e2ae90cfc1b95c58714557f163c0")]
 [assembly: System.Reflection.AssemblyProductAttribute("QuadraticSolver.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QuadraticSolver.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
